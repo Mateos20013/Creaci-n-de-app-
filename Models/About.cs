c58818c1193fee0d0a4195d533fb5481ea3f8a48
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-01 21:23:43 -0500

    Agregar archivos de proyecto.

---
+++ Models/About.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => "Mateo Sotomayor";
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "Hola mi nombre es Mateo Sotomayor, soy un estudiante de Ingeniería de Software apasionado por los drones. Me encanta la emoción de las competencias y siempre intento buscar nuevas formas de mejorar mis habilidades y compartir con gente.";
+    public string Icon { get; set; }
+}
